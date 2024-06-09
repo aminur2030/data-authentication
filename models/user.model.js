4c6a4705f593199822c17a13fd9f81e229a2e9d5
Aminur <aminurrahaman3399@gmail.com>
2024-06-09 12:18:27 -0400

    first commit

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+const userSchema=mongoose.Schema({
+  email:{
+    type:String,
+    require:true
+  },
+  password:{
+    type:String,
+    require:true
+  },
+  createdOn:{
+    type:Date,
+    default:Date.now
+  }
+})
+module.exports=mongoose.model("user",userSchema)
